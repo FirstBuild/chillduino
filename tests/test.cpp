fb28554edcedf41415e79afe0ddf3b5755af08ca
Christopher M. Baker <chris@firstbuild.com>
2015-05-14 11:17:35 -0400

    Unit test thermistor range

--- tests/test.cpp
+++ tests/test.cpp
@@ -25,67 +25,6 @@
 #include <iostream>
 #include <assert.h>
 
-template <typename T, T expected>
-class Expect {
-  private:
-    T _actual;
-
-  public:
-    Expect(void) :
-      _actual() { }
-
-    virtual ~Expect(void) {
-      assert(_actual == expected);
-    }
-
-    T operator()(void) const {
-      return _actual;
-    }
-
-    void operator()(const T& value) {
-      _actual = value;
-    }
-};
-
-template <int X>
-class WhenFreshFoodThermistorReads {
-  public:
-    WhenFreshFoodThermistorReads(void) { }
-    virtual ~WhenFreshFoodThermistorReads(void) { }
-
-    static int getFreshFoodThermistorReading(void) {
-      return X;
-    }
-};
-
-template <int X>
-class ShouldHaveFreshFoodTemperature {
-  public:
-    ShouldHaveFreshFoodTemperature(void) { }
-    virtual ~ShouldHaveFreshFoodTemperature(void) { }
-
-    static Expect<int, X> setFreshFoodTemperature;
-};
-
-template <int X>
-Expect<int, X> ShouldHaveFreshFoodTemperature<X>::setFreshFoodTemperature;
-
-class ShouldSampleFreshFood :
-  public WhenFreshFoodThermistorReads<512>,
-  public ShouldHaveFreshFoodTemperature<25> {
-
-  public:
-    static const unsigned long FRESH_FOOD_THERMISTOR_B = 3980;
-    static const unsigned long FRESH_FOOD_THERMISTOR_ROOM_RESISTANCE = 5000;
-    static const unsigned long FRESH_FOOD_THERMISTOR_1_RESISTANCE = 15000;
-    static const unsigned long FRESH_FOOD_THERMISTOR_2_RESISTANCE = 10000;
-    static const unsigned long FRESH_FOOD_THERMISTOR_VOLTAGE = 5;
-    static const unsigned long FRESH_FOOD_THERMISTOR_ROOM_TEMPERATURE = 25;
-
-    static const int FRESH_FOOD_SAMPLE_FREQUENCY = 100;
-    static const int FRESH_FOOD_SAMPLES_PER_AVERAGE = 10;
-};
-
 class Time {
   public:
     static void elapse(unsigned long ticks) {
@@ -99,17 +38,47 @@ class Time {
 class ShouldReturnNegativeId {
   public:
     static void whenAllTimersAreUsed(void) {
-      while (Application::setTimeout<ShouldReturnNegativeId::dummy>(0) >= 0);
+      while (Application::setTimeout<ShouldReturnNegativeId::empty>(0) >= 0);
       Application::loop();
     }
 
-    static void dummy(void) { }
+    static void empty(void) {
+      
+    }
+};
+
+template <int T, int C>
+class ThermistorUnitTest {
+  public:
+    static const unsigned long FRESH_FOOD_THERMISTOR_B = 3980;
+    static const unsigned long FRESH_FOOD_THERMISTOR_ROOM_RESISTANCE = 5000;
+    static const unsigned long FRESH_FOOD_THERMISTOR_1_RESISTANCE = 15000;
+    static const unsigned long FRESH_FOOD_THERMISTOR_2_RESISTANCE = 10000;
+    static const unsigned long FRESH_FOOD_THERMISTOR_VOLTAGE = 5;
+    static const unsigned long FRESH_FOOD_THERMISTOR_ROOM_TEMPERATURE = 25;
+
+    static const int FRESH_FOOD_SAMPLE_FREQUENCY = 100;
+    static const int FRESH_FOOD_SAMPLES_PER_AVERAGE = 10;
+
+  public:
+    static int getFreshFoodThermistorReading(void) {
+      return T;
+    }
+
+    static void setFreshFoodTemperature(float celsius) {
+      assert(C == (int) celsius);
+    }
+
+    static void setup(void) {
+      Time::elapse(FRESH_FOOD_SAMPLE_FREQUENCY * FRESH_FOOD_SAMPLES_PER_AVERAGE);
+    }
 };
 
 int main(void) {
   ShouldReturnNegativeId::whenAllTimersAreUsed();
-  Chillduino<ShouldSampleFreshFood>::setup();  
-  Time::elapse(1000);
+  Chillduino<ThermistorUnitTest<0, -273> >::setup();  
+  Chillduino<ThermistorUnitTest<512, 25> >::setup();  
+  Chillduino<ThermistorUnitTest<613, 205> >::setup();  
 
   return 0;
 }
